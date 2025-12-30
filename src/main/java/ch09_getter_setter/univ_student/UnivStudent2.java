ac49ff15db4f5ab6825c7b0656ca66571b767bfe
rgn2698-wq <rgn2698@gmail.com>
2025-12-30 17:28:10 +0900

    UnivStudent2

--- src/main/java/ch09_getter_setter/univ_student/UnivStudent2.java
+++ src/main/java/ch09_getter_setter/univ_student/UnivStudent2.java
@@ -1,4 +1,44 @@
 package ch09_getter_setter.univ_student;
 
 public class UnivStudent2 {
+
+    String name;
+    int grade;
+    double score;
+
+    public UnivStudent2(String name) {
+        this.name = name;
+    }
+
+    public UnivStudent2(int grade) {
+        this.grade = grade;
+    }
+
+    public UnivStudent2(double score) {
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
 }
